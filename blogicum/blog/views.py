89910f8beee82e6463ea836043e3ce96818de2e1
Alexey <gasilinalexei@mail.ru>
2024-05-04 23:31:40 +0300

    test_v1

---
+++ blogicum/blog/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+
+from .models import Post, Category
+
+
+def some_posts(post_objects):
+    """Посты из БД"""
+    return post_objects.filter(
+        pub_date__lte=timezone.now(),
+        is_published=True,
+        category__is_published=True
+    )
+
+
+def index(request):
+    """Главная страница"""
+    template = 'blog/index.html'
+    post_list = some_posts(Post.objects).order_by('-pub_date')[:5]
+    context = {'post_list': post_list}
+    return render(request, template, context)
+
+
+def post_detail(request, id):
+    """Описание записи в блоге"""
+    template = 'blog/detail.html'
+    posts = get_object_or_404(some_posts(Post.objects), id=id)
+    context = {'post': posts}
+    return render(request, template, context)
+
+
+def category_posts(request, category_slug):
+    """Публикация категории"""
+    template = 'blog/category.html'
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    post_list = some_posts(category.posts)
+    context = {'category': category, 'post_list': post_list}
+    return render(request, template, context)
